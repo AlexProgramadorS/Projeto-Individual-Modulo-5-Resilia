cb9692310e0c1986606cf2ac6f5ef5658e0cd4c6
AlexProgramadorS <114102366+AlexProgramadorS@users.noreply.github.com>
2023-02-12 02:13:57 -0300

    Upload do arquivo principal
    Upload do arquivo principal (index.js) do projeto facilitando o dia a dia ferramenta no terminal que vai receber uma lista de propriedades de CSS (ex: background-color, font-size, text-align) e vai devolver essa lista ordenada de A-Z. Ordenar o CSS ajuda a encontrar mais rápido a propriedade que precisamos alterar.

---
+++ index.js
@@ -0,0 +1,21 @@
+import readlineSync from "readline-sync";
+
+const lista = [];
+
+var valor = 1;
+
+console.log("Digite 'exibir' ou 'sair' para exbir a listagem e encerrar:")
+
+while(valor !=0){
+    let inserir = readlineSync.question("Digite uma propriedade do CSS: ");
+    inserir = inserir.toLowerCase()
+
+    if (inserir == "sair"){
+        valor = 0 ,console.log("lista CSS:", lista.sort())
+    } else if(inserir == "exibir"){
+        console.log("lista CSS:", lista.sort())
+    } else if(inserir == ""){
+        console.log("Digite uma propriedade do CSS: ")
+    }
+    else (lista.push(inserir))
+}

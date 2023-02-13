32500eca168adb391a227154da7586984eaad475
AlexProgramadorS <114102366+AlexProgramadorS@users.noreply.github.com>
2023-02-13 13:56:25 -0300

    Atualização de código.
    Trocando o valor de while para true.

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ const readlineSync = require("readline-sync");
 
 const lista = [];
 
-var valor = 1;
 
 console.log("Digite 'exibir' ou 'sair' para exbir a listagem e encerrar:")
 
-while(valor !=0){
+while (true) {
     let inserir = readlineSync.question("Digite uma propriedade do CSS: ");
     inserir = inserir.toLowerCase()
 
-    if (inserir == "sair"){
-        valor = 0 ,console.log("lista CSS:", lista.sort())
-    } else if(inserir == "exibir"){
+    if (inserir == "sair") {
         console.log("lista CSS:", lista.sort())
-    } else if(inserir == ""){
+        break;
+    } else if (inserir == "exibir") {
+        console.log("lista CSS:", lista.sort())
+    } else if (inserir == "") {
         console.log("Digite uma propriedade do CSS: ")
-    }
-    else (lista.push(inserir))
+    } else(lista.push(inserir))
 }
